4a9fd69a53084fdf2fc19c1cf33199a9747302d6
Estevao Naval <navalestevao@gmail.com>
2025-01-11 20:45:13 -0300

    - Definindo subnet estático para o docker-compose; - Definindo ipv4 estático para o hostname do seviço do pdf2chemicals worker;

--- docker-pdf2chemicals-worker-entrypoint.sh
+++ docker-pdf2chemicals-worker-entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Criar usuário e adicionar ao grupo
-useradd -m -g $TORQUE_QUEUE_GROUP $TORQUE_USER || true
-
-# Definir senha do usuário
-echo "$TORQUE_USER:$TORQUE_USER_PASS" | chpasswd
-
 trqauthd
 
 celery -A labsoa_website_backend worker --queues=pdf2chemicals_tasks --prefetch-multiplier=1 --autoscale=3,0 --max-tasks-per-child=1 --loglevel=INFO
