f0bb68e19ca931e9b27df9d29a441b3ea5984d2b
estevao_naval <navalestevao@gmail.com>
2025-02-12 11:34:53 -0300

    Removendo serviço set-permissions e corrigindo set-permissions.sh para adicionar usuários de sistema

--- scripts/set-permissions.sh
+++ scripts/set-permissions.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Define o caminho do .env no diretório pai
-$ENV_FILE_PATH="../.env"
+ENV_FILE_PATH="../.env"
 
 # Verifica se o arquivo existe e carrega as variáveis
 if [ -f "$ENV_FILE_PATH" ]; then
-    source $ENV_FILE_PATH
+    . $ENV_FILE_PATH
 else
     echo "File .env not found"
 fi
@@ -23,9 +23,9 @@ fi
 # Create directory if it doesn't exist
 mkdir -p "${DATA_DIR}"
 
-# Create the main "appdata" group if it does not exist
+# Create the main data group if it does not exist
 if ! getent group "${GROUP_NAME}" >/dev/null 2>&1; then
-    addgroup --no-backup -g "${DATA_GID}" "${GROUP_NAME}"
+    addgroup --gid ${DATA_GID} ${GROUP_NAME}
 fi
 
 # Create users with their own groups and add them to the shared group
@@ -35,20 +35,20 @@ for USER_INFO in ${USERS}; do
 
     # Create a group with the same GID as the user's UID (if it doesn’t exist)
     if ! getent group "${USER}" >/dev/null 2>&1; then
-        addgroup --no-backup  -g "${USER_UID}" "${USER}"
+        addgroup --gid ${USER_UID} ${USER}
     fi
 
     # Create the user with the same UID and primary group
     if ! id "${USER}" >/dev/null 2>&1; then
-        adduser  -H -u "${USER_UID}" -G "${USER}" "${USER}"
+        adduser --system --no-create-home --disabled-password --uid ${USER_UID} --gid ${USER_UID} ${USER}
     fi
 
-    # Ensure the user is added to the shared "appdata" group
-    addgroup --no-backup "${USER}" "${GROUP_NAME}" || true
+    # Ensure the user is added to the shared data group
+    sudo usermod -aG ${GROUP_NAME} ${USER}
 done
 
 # Set the group ownership of the shared directory
-chown -R :"${GROUP_NAME}" "${DATA_DIR}"
+chown -R :${GROUP_NAME} "${DATA_DIR}"
 
 # Set permissions to 2775 (setgid activated for group inheritance)
 chmod -R 2775 "${DATA_DIR}"
@@ -57,9 +57,9 @@ chmod -R 2775 "${DATA_DIR}"
 find "${DATA_DIR}" -type d -exec chmod g+s {} +
 
 # Configure default ACL to ensure permission inheritance
-# setfacl -d -m g::rwx "${DATA_DIR}"  # Group always has rwx
-# setfacl -d -m o::rx "${DATA_DIR}"   # Others have only read and execute
-# setfacl -m g::rwx "${DATA_DIR}"     # Apply immediately to existing files
+setfacl -d -m g::rwx "${DATA_DIR}"  # Group always has rwx
+setfacl -d -m o::rx "${DATA_DIR}"   # Others have only read and execute
+setfacl -m g::rwx "${DATA_DIR}"     # Apply immediately to existing files
 
 # Display final status
 ls -ld "${DATA_DIR}"
