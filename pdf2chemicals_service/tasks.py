0cce4574f7a1c6079211f32af682960b462c5b48
estevao_naval <navalestevao@gmail.com>
2025-02-13 01:20:16 -0300

    Modificando comando de envio do pbs_script

--- pdf2chemicals_service/tasks.py
+++ pdf2chemicals_service/tasks.py
@@ -119,7 +119,7 @@ def send_pdf2chemicals_hpc_task(self, *args, **kwargs):
         node_name=node_name
     )
     
-    cmd = f'{os.getenv("TORQUE_HOME")}/bin/qsub {script_path}'
+    cmd = f'sh -c "(cd {os.getenv("TORQUE_USER_HOME")} && {os.getenv("TORQUE_HOME")}/bin/qsub {script_path})"'
     
     result = subprocess.run(
         cmd,
